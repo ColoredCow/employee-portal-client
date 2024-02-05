9854ddf36b543e62e683c92e00ddd9b760d3f764
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2024-02-05 18:02:52 +0530

    ci: updated ci file

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx,css,md}': (filenames) => ['npm run validate']
+  '*.{ts,tsx,js,jsx,css,md}': (filenames) => [
+    'npm run format:fix',
+    'npm run lint:fix',
+    'git add .',
+    'npm run lint',
+    'npm run format:check',
+    'npm run type:check'
+  ]
 }
